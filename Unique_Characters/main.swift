2381f61bd1dd60b89cedf9372f7c045f1c9d177f
Andy <ajlewis1@me.com>
2018-07-02 20:04:34 -0600

    Initial Commit

---
+++ Unique_Characters/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Unique_Characters
+//
+//  Created by Andrew Lewis on 7/2/18.
+//  Copyright © 2018 Andrew Lewis. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
